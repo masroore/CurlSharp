df131045c188f3efb90c1549b55f81f1717ff74f
dominikhura <dhura@hidglobal.com>
2014-11-27 13:47:42 +0100

    Revert "Copying of curl libraries to samples on post built event"
    This reverts commit 687e988731b5d01dfa2bdad126de975fed1fb1ba.

--- Samples/EasyPost/EasyPost.cs
+++ Samples/EasyPost/EasyPost.cs
@@ -10,7 +10,7 @@ namespace EasyPost
     {
         // Upload the provided PHP script (testpost.php) to webroot and modify the 
         // following URL accordingly
-        private const string TEST_URL = "http://www.wp.pl";
+        private const string TEST_URL = "http://localhost/testpost.php";
 
         private static void Main(string[] args)
         {
