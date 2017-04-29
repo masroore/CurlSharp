ef13686a22add80a5ba4dee4e6b126f220644f9d
Przemysław Ryciuk <p@ryciuk.com>
2017-04-29 22:47:53 +0200

    Port EasyPost sample from WebRequest to modern HttpClient

--- Samples/EasyPost/EasyPost.cs
+++ Samples/EasyPost/EasyPost.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using CurlSharp;
 
@@ -10,11 +10,12 @@ namespace EasyPost
     {
         // Upload the provided PHP script (testpost.php) to webroot and modify the 
         // following URL accordingly
-        private const string TEST_URL = "http://localhost/testpost.php";
+        // private const string TEST_URL = "http://localhost/testpost.php";
+        private const string TEST_URL = "http://httpbin.org/post";
 
         private static void Main(string[] args)
         {
-            var bits = IntPtr.Size*8;
+            var bits = IntPtr.Size * 8;
             Console.WriteLine("Test curl {0} bit", bits);
             Curl.GlobalInit(CurlInitFlag.All);
             Console.WriteLine("Curl Version: {0}\n", Curl.Version);
@@ -22,25 +23,20 @@ namespace EasyPost
             const string postData = "parm1=12345&parm2=Hello+world%21";
             var postLength = postData.Length;
 
-            Console.WriteLine("\n========== TEST 1 HttpWebRequest ============");
+            Console.WriteLine("\n========== TEST 1 HttpClient ============");
 
-            var request = (HttpWebRequest) WebRequest.Create(TEST_URL);
-            var data = Encoding.ASCII.GetBytes(postData);
-            request.UserAgent = "HttpWebRequest";
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.ContentLength = data.Length;
-
-            using (var stream = request.GetRequestStream())
+            using (var httpClient = new HttpClient())
             {
-                stream.Write(data, 0, data.Length);
+                var request = new HttpRequestMessage(HttpMethod.Post, TEST_URL);
+                request.Headers.Add("User-Agent", "HttpClient");
+                request.Content = new StringContent(postData);
+                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+
+                var response = httpClient.SendAsync(request).Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+
+                Console.WriteLine(responseString);
             }
-
-            var response = (HttpWebResponse) request.GetResponse();
-            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-
-            Console.WriteLine(responseString);
-
 
             try
             {
@@ -63,11 +59,11 @@ namespace EasyPost
 
                 var mf = new CurlHttpMultiPartForm();
                 mf.AddSection(CurlFormOption.CopyName, "parm1",
-                              CurlFormOption.CopyContents, "12345",
-                              CurlFormOption.End);
+                    CurlFormOption.CopyContents, "12345",
+                    CurlFormOption.End);
                 mf.AddSection(CurlFormOption.CopyName, "parm2",
-                              CurlFormOption.CopyContents, "Hello world!",
-                              CurlFormOption.End);
+                    CurlFormOption.CopyContents, "Hello world!",
+                    CurlFormOption.End);
                 using (var easy = new CurlEasy())
                 {
                     easy.WriteFunction = OnWriteData;
@@ -93,14 +89,14 @@ namespace EasyPost
         public static Int32 OnWriteData(Byte[] buf, Int32 size, Int32 nmemb, Object extraData)
         {
             Console.Write(Encoding.UTF8.GetString(buf));
-            return size*nmemb;
+            return size * nmemb;
         }
 
         public static Int32 OnHeaderData(Byte[] buf, Int32 size, Int32 nmemb, Object extraData)
         {
             var userData = (string) extraData;
             Console.Write("->" + Encoding.UTF8.GetString(buf));
-            return size*nmemb;
+            return size * nmemb;
         }
 
         public static void OnDebug(CurlInfoType infoType, String msg, Object extraData)
@@ -108,6 +104,6 @@ namespace EasyPost
             // print out received data only
             if (infoType == CurlInfoType.DataIn)
                 Console.WriteLine(msg);
+            }
         }
     }
-}
